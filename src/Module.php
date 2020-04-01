fc814b6ee2d9f41f33356ed5e55942ec4f4d3ce5
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2020-04-01 15:51:31 +0700

    add bs4

--- src/Module.php
+++ src/Module.php
@@ -3,6 +3,7 @@
 namespace diecoding\rbac;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 
 /**
  * @inheritdoc
@@ -24,6 +25,27 @@ class Module extends \mdm\admin\Module
      * @inheritdoc
      */
     public $layout = 'left-menu';
+
+    /**
+     * @var int|string the bootstrap library version.
+     *
+     * To use with bootstrap 3 - you can set this to any string starting with 3 (e.g. `3` or `3.3.7` or `3.x`)
+     * To use with bootstrap 4 - you can set this to any string starting with 4 (e.g. `4` or `4.1.1` or `4.x`)
+     *
+     * This property can be set up globally in Yii application params in your Yii2 application config file.
+     *
+     * For example:
+     * `Yii::$app->params['bsVersion'] = '4.x'` to use with Bootstrap 4.x globally
+     *
+     * If this property is set, this setting will override the `Yii::$app->params['bsVersion']`. If this is not set, and
+     * `Yii::$app->params['bsVersion']` is also not set, this will default to `3.x` (Bootstrap 3.x version).
+     */
+    public $bsVersion;
+
+    /**
+     * @var bool flag to detect whether bootstrap 4.x version is set
+     */
+    protected $_isBs4;
 
     /**
      * @inheritdoc
@@ -50,5 +72,64 @@ class Module extends \mdm\admin\Module
         ];
 
         $this->setViewPath('@mdm/admin/views');
+    }
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param \yii\base\Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        $config = [];
+        if ($this->isBs4()) {
+            $config = [
+                'components' => [
+                    'assetManager' => [
+                        'bundles' => [
+                            'yii\bootstrap\BootstrapAsset' => [],
+                            'yii\bootstrap\BootstrapPluginAsset' => [],
+                        ],
+                    ],
+                ],
+            ];
+        }
+        Yii::configure($app, $config);
+    }
+
+    /**
+     * Configures the bootstrap version settings
+     * @return string the bootstrap lib parsed version number
+     */
+    protected function configureBsVersion()
+    {
+        $v = empty($this->bsVersion) ? ArrayHelper::getValue(Yii::$app->params, 'bsVersion', '3') : $this->bsVersion;
+        $ver = static::parseVer($v);
+        $this->_isBs4 = $ver === '4';
+        return $ver;
+    }
+
+    /**
+     * Validate if Bootstrap 4.x version
+     * @return bool
+     *
+     * @throws InvalidConfigException
+     */
+    public function isBs4()
+    {
+        if (!isset($this->_isBs4)) {
+            $this->configureBsVersion();
+        }
+        return $this->_isBs4;
+    }
+
+    /**
+     * Parses and returns the major BS version
+     * @param string $ver
+     * @return bool|string
+     */
+    protected static function parseVer($ver)
+    {
+        $ver = (string) $ver;
+        return substr(trim($ver), 0, 1);
     }
 }
